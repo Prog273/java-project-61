aab7e6ac894d7ec78ea918fa62efdaf7b0f2259f
Prog273 <dimitrius273@mail.ru>
2024-06-30 00:48:36 +0300

    Add Prime.java

---
+++ app/src/main/java/hexlet/code/games/Prime.java
@@ -0,0 +1,54 @@
+package hexlet.code.games;
+
+import hexlet.code.Engine;
+
+import java.util.Random;
+import java.util.Scanner;
+
+public class Prime {
+    public static void prime() {
+        String userName = Engine.getUserName();
+        System.out.println("Answer 'yes' if given number is prime. Otherwise answer 'no'.");
+        Scanner scanner = new Scanner(System.in);
+        Random random = new Random();
+        for (int i = 0; i < 3; i++) {
+            int numberToCheck = random.nextInt(99) + 2;
+            System.out.println("Question: " + numberToCheck);
+            String answer = scanner.next();
+            System.out.println("Your answer: " + answer);
+            if ((isNumberPrime(numberToCheck) && answer.equals("yes"))
+                    || (!isNumberPrime(numberToCheck) && answer.equals("no"))) {
+                System.out.println("Correct!");
+            } else if (isNumberPrime(numberToCheck) && answer.equals("no")) {
+                System.out.println("'" + answer + "' is wrong answer ;(. Correct answer was 'yes'"
+                        + "\nLet's try again, " + userName + "!");
+                break;
+            } else if (!isNumberPrime(numberToCheck) && answer.equals("yes")) {
+                System.out.println("'" + answer + "' is wrong answer ;(. Correct answer was 'no'"
+                        + "\nLet's try again, " + userName + "!");
+                break;
+            } else {
+                System.out.println("'" + answer + "' is wrong answer ;(."
+                        + "\nLet's try again, " + userName + "!");
+                break;
+            }
+            if (i == 2) {
+                System.out.println("Congratulations, " + userName + "!");
+            }
+        }
+    }
+
+    public static boolean isNumberPrime(int number) {
+        if (number == 1) {
+            return false;
+        }
+        int divider = 2;
+        while (Math.pow(divider, 2) <= number) {
+            if (number % divider == 0) {
+                return false;
+            }
+            divider++;
+        }
+        return true;
+    }
+}
