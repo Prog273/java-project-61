cec992a6ee27a0e8758b4ae3a2e7955841ac6f74
Prog273 <dimitrius273@mail.ru>
2024-07-27 17:18:24 +0300

    Update App.java, Engine.java

--- app/src/main/java/hexlet/code/Engine.java
+++ app/src/main/java/hexlet/code/Engine.java
@@ -1,11 +1,5 @@
 package hexlet.code;
 
-import hexlet.code.games.CalcGame;
-import hexlet.code.games.EvenGame;
-import hexlet.code.games.GcdGame;
-import hexlet.code.games.PrimeGame;
-import hexlet.code.games.ProgressionGame;
-
 import java.util.ArrayList;
 import java.util.Scanner;
 
